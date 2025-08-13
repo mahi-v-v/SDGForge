b69d127dafc7934bf1f7257755a5862ef4a3fcf0
Athira P <athirap930@gmail.com>
2025-08-13 08:41:45 +0530

    fix: correct wording in documentation text

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,8 +25,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite & React logos to learn more
       </p>
     </>
   )
